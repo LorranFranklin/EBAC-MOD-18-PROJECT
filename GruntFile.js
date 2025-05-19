8e9a2ca10bc677f59bb1007b209f599486097533
Lorran <lorranfranklin.dev@gmail.com>
2025-05-19 09:07:53 -0300

    att project

--- GruntFile.js
+++ GruntFile.js
@@ -1,7 +1,25 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+        files: {
+          'dev/styles/main.css': 'src/styles/main.less', // Compile main.less to main.css
+        },
+      },
+      production: {
+        options: {
+          compress: true, // Compress the CSS output
+        },
+        files: {
+          'dist/styles/main.min.css': 'src/styles/main.less', // Compile and minify main.less to main.min.css
+        },
+      },
+    },
   });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   grunt.registerTask('olaGrunt', function () {
     const done = this.async(); // Mark the task as asynchronous
@@ -11,5 +29,6 @@ module.exports = function (grunt) {
     }, 3000); // Wait for 3 seconds before logging the message
   });  
 
-  grunt.registerTask('default', ['olaGrunt']);   // Register the default task
+  grunt.registerTask('default', ['less:development']);   // Register the default task
+  grunt.registerTask('build', ['less:production']);
 }
